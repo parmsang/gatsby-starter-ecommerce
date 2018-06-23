f81fdef8a76abd98919e3da6e6bf826d5796a3b0
parminder <parminder.sanghera@connet-group.com>
2018-06-23 18:12:00 +0100

    updates favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Container } from 'semantic-ui-react'
 import Headroom from 'react-headroom'
 import Helmet from 'react-helmet'
+import { withPrefix } from 'gatsby-link'
 import 'semantic-ui-css/semantic.min.css'
 import AuthProvider from '../components/Context/AuthProvider'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import favicon from './favicon.ico'
 
 class Layout extends React.PureComponent {
   componentDidMount() {
@@ -25,13 +25,35 @@ class Layout extends React.PureComponent {
 
     return (
       <AuthProvider>
-        <Helmet>
+        <Helmet
+          meta={[
+            {
+              name: 'description',
+              content: 'A starter eCommerce website made using GatsbyJS',
+            },
+            { name: 'keywords', content: 'ecommerce, gatsby, moltin' },
+            { name: 'msapplication-TileColor', content: '#da532c' },
+            { name: 'theme-color', content: '#ffffff' },
+          ]}
+        >
           <html lang="en" />
-          <meta
-            name="description"
-            content="A starter eCommerce website made using GatsbyJS"
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={withPrefix('/favicons/apple-touch-icon.png')}
           />
-          <link rel="shortcut icon" href={favicon} />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={withPrefix('/favicons/favicon-32x32.png')}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={withPrefix('/favicons/favicon-16x16.png')}
+          />
         </Helmet>
         <Headroom upTolerance={10} downTolerance={10} style={{ zIndex: '20' }}>
           <Header location={location} />
