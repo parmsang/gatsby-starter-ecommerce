aca242df5ca1c2af46d9de2814660ea57af21cd9
Parminder Sanghera <sanghera.p@gmail.com>
2019-03-09 11:09:58 +0000

    added and set up husky and lint-staged

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,77 @@
-/* eslint-disable */
-
 import React from 'react'
-import {graphql, useStaticQuery} from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import get from 'lodash/get'
-import {Image, Header} from 'semantic-ui-react'
+import { Image, Header } from 'semantic-ui-react'
 import Helmet from 'react-helmet'
 import ProductList from '../components/ProductList'
 import logo from '../images/ill-short-dark.svg'
 import Layout from '../components/Layout'
 
-export default StoreIndex = (props) => {
-
-  const data = useStaticQuery(graphql `
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
+const StoreIndex = ({ location }) => {
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      site {
+        siteMetadata {
+          title
+        }
       }
-    }
-    allMoltinProduct {
-      edges {
-        node {
-          id
-          name
-          description
-          background_colour
-          new
-          mainImageHref
-          meta {
-            display_price {
-              with_tax {
-                amount
-                currency
-                formatted
+      allMoltinProduct {
+        edges {
+          node {
+            id
+            name
+            description
+            background_colour
+            new
+            mainImageHref
+            meta {
+              display_price {
+                with_tax {
+                  amount
+                  currency
+                  formatted
+                }
               }
             }
-          }
-          mainImage {
-            childImageSharp {
-              sizes(maxWidth: 600) {
-                ...GatsbyImageSharpSizes
+            mainImage {
+              childImageSharp {
+                sizes(maxWidth: 600) {
+                  ...GatsbyImageSharpSizes
+                }
               }
             }
           }
         }
       }
     }
-  }
   `)
 
   const siteTitle = get(data, 'site.siteMetadata.title')
   const products = get(data, 'allMoltinProduct.edges')
   const filterProductsWithoutImages = products.filter(v => v.node.mainImageHref)
   return (
-    <Layout location={props.location}>
-      <Helmet title={siteTitle}/>
+    <Layout location={location}>
+      <Helmet title={siteTitle} />
       <Header
         as="h3"
         icon
         textAlign="center"
         style={{
-        marginBottom: '2em'
-      }}>
+          marginBottom: '2em',
+        }}
+      >
         <Header.Content
           style={{
-          width: '60%',
-          margin: '0 auto'
-        }}>
-          <Image src={logo} alt={'logo'}/>
+            width: '60%',
+            margin: '0 auto',
+          }}
+        >
+          <Image src={logo} alt="logo" />
         </Header.Content>
       </Header>
-      <ProductList products={filterProductsWithoutImages}/>
+      <ProductList products={filterProductsWithoutImages} />
     </Layout>
   )
 }
+
+export default StoreIndex
