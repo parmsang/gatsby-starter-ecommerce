e274ba85a85e9fad3f0ca1041424e935e05d46d1
Parminder Sanghera <sanghera.p@gmail.com>
2019-03-09 13:22:04 +0000

    chore: further jest configuration to get test working

---
+++ src/pages/__tests__/register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import Register from '../register'
+
+const props = { location: { pathname: '' } }
+
+test('register renders name and email', () => {
+  const { getByLabelText } = render(<Register {...props} />)
+
+  expect(getByLabelText(/Email/i))
+  expect(getByLabelText(/name/i))
+})
