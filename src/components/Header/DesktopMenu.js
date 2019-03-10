90bc684b64ad5311ea65359ec13c0227152d3822
Parminder Sanghera <sanghera.p@gmail.com>
2019-03-10 13:46:02 +0000

    refactor: use react hooks for DesktopMenu component

--- src/components/Header/DesktopMenu.js
+++ src/components/Header/DesktopMenu.js
@@ -1,87 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, withPrefix } from 'gatsby'
 import { Menu, Container, Icon } from 'semantic-ui-react'
 import ShoppingCartIcon from './ShoppingCartIcon'
 import Logo from './Logo'
 
-class DesktopMenu extends Component {
-  state = {
-    activeItem: this.props.location.pathname,
-  }
+const DesktopMenu = ({ location: { pathname }, token, cartCount }) => {
+  const [activeItem, setActiveItem] = useState(pathname)
 
-  componentWillReceiveProps(nextProps) {
-    const nextPathname = nextProps.location.pathname
-    const currentPathname = this.props.location.pathname
+  useEffect(() => {
+    setActiveItem(pathname)
+  }, [pathname])
 
-    if (nextPathname !== currentPathname) {
-      this.setState({
-        activeItem: nextPathname,
-      })
-    }
-  }
-
-  render() {
-    const { activeItem } = this.state
-    const { token, cartCount } = this.props
-    return (
-      <Menu size="huge" borderless pointing>
-        <Container text>
-          <Menu.Item
-            active={activeItem === withPrefix('/')}
-            as={Link}
-            to="/"
-            header
-          >
-            <Logo />
-            Starter Store
-          </Menu.Item>
-          {token ? (
-            <Menu.Menu position="right">
-              <Menu.Item
-                as={Link}
-                to="/myaccount/"
-                active={activeItem === withPrefix('/myaccount/')}
-              >
-                <Icon name="user" />
-                My Account
-              </Menu.Item>
-              <Menu.Item
-                as={Link}
-                to="/cart/"
-                active={activeItem === withPrefix('/cart/')}
-              >
-                <ShoppingCartIcon cartCount={cartCount} name="Cart" />
-              </Menu.Item>
-            </Menu.Menu>
-          ) : (
-            <Menu.Menu position="right">
-              <Menu.Item
-                as={Link}
-                to="/register/"
-                active={activeItem === withPrefix('/register/')}
-              >
-                Sign up
-              </Menu.Item>
-              <Menu.Item
-                as={Link}
-                to="/login/"
-                active={activeItem === withPrefix('/login/')}
-              >
-                Sign in
-              </Menu.Item>
-              <Menu.Item
-                as={Link}
-                to="/cart/"
-                active={activeItem === withPrefix('/cart/')}
-              >
-                <ShoppingCartIcon cartCount={cartCount} name="Cart" />
-              </Menu.Item>
-            </Menu.Menu>
-          )}
-        </Container>
-      </Menu>
-    )
-  }
+  return (
+    <Menu size="huge" borderless pointing>
+      <Container text>
+        <Menu.Item
+          active={activeItem === withPrefix('/')}
+          as={Link}
+          to="/"
+          header
+        >
+          <Logo />
+          Starter Store
+        </Menu.Item>
+        {token ? (
+          <Menu.Menu position="right">
+            <Menu.Item
+              as={Link}
+              to="/myaccount/"
+              active={activeItem === withPrefix('/myaccount/')}
+            >
+              <Icon name="user" />
+              My Account
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/cart/"
+              active={activeItem === withPrefix('/cart/')}
+            >
+              <ShoppingCartIcon cartCount={cartCount} name="Cart" />
+            </Menu.Item>
+          </Menu.Menu>
+        ) : (
+          <Menu.Menu position="right">
+            <Menu.Item
+              as={Link}
+              to="/register/"
+              active={activeItem === withPrefix('/register/')}
+            >
+              Sign up
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/login/"
+              active={activeItem === withPrefix('/login/')}
+            >
+              Sign in
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/cart/"
+              active={activeItem === withPrefix('/cart/')}
+            >
+              <ShoppingCartIcon cartCount={cartCount} name="Cart" />
+            </Menu.Item>
+          </Menu.Menu>
+        )}
+      </Container>
+    </Menu>
+  )
 }
 
 export default DesktopMenu
