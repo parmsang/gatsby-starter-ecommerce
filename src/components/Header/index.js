89c0580d77c5a95e41d9c3e9e0c03503df88b40e
Parminder Sanghera <sanghera.p@gmail.com>
2019-03-10 13:52:16 +0000

    refactor: use react hooks for Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Responsive } from 'semantic-ui-react'
 import DesktopMenu from './DesktopMenu'
 import MobileMenu from './MobileMenu'
 import AuthContext from '../Context/AuthContext'
 import CartContext from '../Context/CartContext'
 
-class Header extends React.PureComponent {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {auth => (
-          <CartContext.Consumer>
-            {cart => (
-              <React.Fragment>
-                <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
-                  <MobileMenu
-                    location={this.props.location}
-                    token={auth.token}
-                    cartCount={cart.cartCount}
-                  />
-                </Responsive>
-                <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-                  <DesktopMenu
-                    location={this.props.location}
-                    token={auth.token}
-                    cartCount={cart.cartCount}
-                  />
-                </Responsive>
-              </React.Fragment>
-            )}
-          </CartContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    )
-  }
+const Header = ({ location }) => {
+  const { cartCount } = useContext(CartContext)
+  const { token } = useContext(AuthContext)
+
+  return (
+    <>
+      <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+        <MobileMenu location={location} token={token} cartCount={cartCount} />
+      </Responsive>
+      <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+        <DesktopMenu location={location} token={token} cartCount={cartCount} />
+      </Responsive>
+    </>
+  )
 }
 
 export default Header
