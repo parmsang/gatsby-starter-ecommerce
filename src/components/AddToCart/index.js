8434df01fe75c84f2584054081748d6e2960953e
Parminder Sanghera <sanghera.p@gmail.com>
2019-03-10 13:10:32 +0000

    reformat: use react hooks for AddToCart component

--- src/components/AddToCart/index.js
+++ src/components/AddToCart/index.js
@@ -1,64 +1,23 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Input, Icon, Transition } from 'semantic-ui-react'
 import CartContext from '../Context/CartContext'
 
 const Moltin = require('../../../lib/moltin')
 
-export default class AddToCart extends React.Component {
-  state = {
-    error: '',
-    loading: false,
-    quantity: 1,
-    visible: false,
-  }
+const AddToCart = ({ productId }) => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [quantity, setQuantity] = useState(1)
+  const [visible, setVisible] = useState(false)
+  const { addToCart } = useContext(CartContext)
 
-  _handleSubmit = (e, context) => {
-    const { productId } = this.props
-    const { quantity } = this.state
-    const cartId = localStorage.getItem('mcart')
-
-    const error = this.validate(quantity)
-    this.setState({ error })
-    if (!error) {
-      this.setState({
-        loading: true,
-      })
-
-      Moltin.addToCart(cartId, productId, quantity)
-        .then(() => {
-          context.addToCart(quantity, cartId)
-
-          this.setState(
-            {
-              loading: false,
-              quantity: 1,
-              visible: true,
-            },
-            this.toggleMessage()
-          )
-        })
-        .catch(() =>
-          this.setState({
-            error: 'Something went wrong',
-            loading: false,
-          })
-        )
-    }
-  }
-
-  toggleMessage = () => {
+  const toggleMessage = () => {
     setTimeout(() => {
-      this.setState({ visible: false })
+      setVisible(false)
     }, 1000)
   }
 
-  _handleChange = ({ target: { value } }) => {
-    this.setState({
-      quantity: value,
-    })
-  }
-
-  validate = quantity => {
+  const validate = quantity => {
     let error
     const re = /^[0-9\b]+$/
 
@@ -68,41 +27,60 @@ export default class AddToCart extends React.Component {
     return error
   }
 
-  render() {
-    const { loading, quantity, visible, error } = this.state
-    return (
-      <CartContext.Consumer>
-        {context => (
-          <React.Fragment>
-            <Input
-              type="number"
-              placeholder="Quantity"
-              value={quantity}
-              min={1}
-              step={1}
-              error={!!error}
-              onChange={e => this._handleChange(e)}
-              action={{
-                color: 'orange',
-                content: 'Add to Cart',
-                icon: 'plus cart',
-                onClick: e => this._handleSubmit(e, context),
-                loading,
-                disabled: loading,
-              }}
-            />
-            {error && (
-              <div style={{ color: 'red', position: 'absolute' }}>{error}</div>
-            )}
-            <Transition duration={{ hide: 500, show: 500 }} visible={visible}>
-              <div style={{ color: 'green', position: 'absolute' }}>
-                <Icon name="check" />
-                Added to cart
-              </div>
-            </Transition>
-          </React.Fragment>
-        )}
-      </CartContext.Consumer>
-    )
+  const handleSubmit = () => {
+    const cartId = localStorage.getItem('mcart')
+
+    const error = validate(quantity)
+    setError(error)
+    if (!error) {
+      setLoading(true)
+      Moltin.addToCart(cartId, productId, quantity)
+        .then(() => {
+          addToCart(quantity, cartId)
+          setLoading(false)
+          setQuantity(1)
+          setVisible(true)
+          toggleMessage()
+        })
+        .catch(() => {
+          setError('Something went wrong')
+          setLoading(false)
+        })
+    }
   }
+
+  const handleChange = ({ target: { value } }) => setQuantity(value)
+
+  return (
+    <>
+      <Input
+        type="number"
+        placeholder="Quantity"
+        value={quantity}
+        min={1}
+        step={1}
+        error={!!error}
+        onChange={handleChange}
+        action={{
+          color: 'orange',
+          content: 'Add to Cart',
+          icon: 'plus cart',
+          onClick: handleSubmit,
+          loading,
+          disabled: loading,
+        }}
+      />
+      {error && (
+        <div style={{ color: 'red', position: 'absolute' }}>{error}</div>
+      )}
+      <Transition duration={{ hide: 500, show: 500 }} visible={visible}>
+        <div style={{ color: 'green', position: 'absolute' }}>
+          <Icon name="check" />
+          Added to cart
+        </div>
+      </Transition>
+    </>
+  )
 }
+
+export default AddToCart
